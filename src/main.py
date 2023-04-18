6db28af8284205eb7579d5bc374113553948cacd
sfq121 <9960121@>
2023-04-18 20:39:57 +0800

    冒烟通过

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
+#import Utils as Utils
+from src.Utils import SendMail
+
+
 class mailHack:
     def __init__(self):
-        self.fakeAddress = ""
-        self.fakeName = " "
+        self.sender = "1448117242@qq.com"
+        self.subject = "aa"
+        self.mail_pass = "daqyavgqmwcbbaaj"
         self.attNum = ""
+        self.mail_host = "smtp.qq.com"
+        self.mail_user = "1448117242@qq.com"
+        self.enclosurepath = ""
+        self.header = ""
+
+    def main(self):
+        SendMail.sendmail.sendMail(self,self.sender,self.subject,self.mail_pass,self.mail_host,self.mail_user,self.header,self.enclosurepath)
+
+
+        
+if __name__ == '__main__':
+     mailhack = mailHack()
+     mailhack.main()
